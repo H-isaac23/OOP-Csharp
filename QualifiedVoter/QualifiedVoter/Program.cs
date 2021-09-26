eb7ba82605003d6cec6791faf36396380c9d587a
H-isaac23 <dev.isaac23@gmail.com>
2021-09-26 21:58:15 +0800

    voter

---
+++ QualifiedVoter/QualifiedVoter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QualifiedVoter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if(age < 18)
+            {
+                Console.WriteLine("You are not yet qualified to vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are qualified to vote!");
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
