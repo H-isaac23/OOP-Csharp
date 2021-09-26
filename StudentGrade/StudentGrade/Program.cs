b31a90a757c33db4e533cf847b08fe21fac0e1f8
H-isaac23 <dev.isaac23@gmail.com>
2021-09-26 21:58:34 +0800

    StudentGrade/

---
+++ StudentGrade/StudentGrade/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your grade: ");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("A");
+            }else if (grade >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else if (grade < 60)
+            {
+                Console.WriteLine("E");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
