97ae0385da13cba6e964f07d3f312dfa555ca0ab
H-isaac23 <dev.isaac23@gmail.com>
2021-09-26 21:57:34 +0800

    delfah

---
+++ CelsiusToFahrenheit/CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter temperature in Fahrenheit: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            double celsius = (5 / 9.0) * (temp - 32);
+            Console.WriteLine("\n" + temp + " fahrenheit in celsius is: " + celsius);
+            Console.ReadLine();
+        }
+    }
+}
