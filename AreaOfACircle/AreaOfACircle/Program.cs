af81095a76e3a3aaab398acb887b1b262e3e8815
H-isaac23 <dev.isaac23@gmail.com>
2021-09-26 21:57:22 +0800

    area circle

---
+++ AreaOfACircle/AreaOfACircle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter radius: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            float pi = 3.1416f;
+            float area = r * pi * pi;
+            Console.WriteLine("The area of the circle with radius " + r + " is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
