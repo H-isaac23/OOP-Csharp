dba9c6b628e3bb0de860455a901aab1883aa6d45
H-isaac23 <dev.isaac23@gmail.com>
2021-09-26 21:58:42 +0800

    sumnum

---
+++ SumTwoNumbers/SumTwoNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum of the two numbers is: " + (num1 + num2));
+            Console.ReadLine();
+        }
+    }
+}
