9dba94d3dcb7168ac9f1f3242e3bfcac53a47bda
H-isaac23 <dev.isaac23@gmail.com>
2021-11-25 23:27:21 +0800

    some shit idk

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] x = new Shape[1];
+            x[0] = new Shape();
+
+        }
+    }
+
+    class Shape
+    {
+        public Shape(){
+            name = "Hi";
+        }
+
+        public String name
+        {
+            get; set;
+        }
+        public void draw()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+
+    class Triangle : Shape {
+        public new void draw()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+}
